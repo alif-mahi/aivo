b9c719f22f2aaff276b40704080f5e4a73b66690
Alif Jawad Mahi <mahi.alifjawad@gmail.com>
2025-11-10 19:17:02 +0600

    Add Agent Update and Delete
    Added:
    1. `agents.remove` procedure.
    2. `agents.update` procedure.
    3. Edit UI.
    4. Delete UI.

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -1,131 +1,121 @@
-import { useTRPC } from "@/trpc/client";
-import { AgentGetOne } from "../../types";
-import { useRouter } from "next/navigation";
+import { agentsInsertSchema } from "@/modules/agents/schemas";
+import { AgentGetOne } from "@/modules/agents/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { agentsInsertSchema } from "../../schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+
+import { useTRPC } from "@/trpc/client";
 import { GeneratedAvatar } from "@/components/generated-avater";
-import { Textarea } from "@/components/ui/textarea";
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { toast } from "sonner";
 
 interface AgentFormProps {
-  onSuccess?: () => void;
-  onCancel?: () => void;
-  initialValues?: AgentGetOne;
+    onSuccess?: () => void;
+    onCancel?: () => void;
+    initialValues?: AgentGetOne;
 }
 
-export const AgentForm = ({
-  onSuccess,
-  onCancel,
-  initialValues,
-}: AgentFormProps) => {
-  const trpc = useTRPC();
-  const router = useRouter();
-  const queryClient = useQueryClient();
+export const AgentForm = ({ onSuccess, onCancel, initialValues }: AgentFormProps) => {
+    const trpc = useTRPC();
 
-  const createAgent = useMutation(
-    trpc.agents.create.mutationOptions({
-      onSuccess: () => {
-        queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
-        if (initialValues?.id) {
-          queryClient.invalidateQueries(
-            trpc.agents.getOne.queryOptions({ id: initialValues.id }),
-          );
+    const queryClient = useQueryClient();
+
+    const createAgent = useMutation(
+        trpc.agents.create.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
+
+                onSuccess?.();
+            },
+            onError: (error) => {
+                toast.message(error.message);
+            },
+        })
+    );
+
+    const updateAgent = useMutation(
+        trpc.agents.update.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
+                if (initialValues?.id) {
+                    await queryClient.invalidateQueries(trpc.agents.getOne.queryOptions({ id: initialValues.id }));
+                }
+                onSuccess?.();
+            },
+            onError: (error) => {
+                toast.message(error.message);
+            },
+        })
+    );
+
+    const form = useForm<z.infer<typeof agentsInsertSchema>>({
+        resolver: zodResolver(agentsInsertSchema),
+        defaultValues: {
+            name: initialValues?.name ?? "",
+            instructions: initialValues?.instructions ?? "",
+        },
+    });
+
+    const isEdit = !!initialValues?.id;
+    const isPending = createAgent.isPending || updateAgent.isPending;
+
+    const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
+        if (isEdit) {
+            updateAgent.mutate({ ...values, id: initialValues.id });
+        } else {
+            createAgent.mutate(values);
         }
-        onSuccess?.();
-      },
-      onError: (error) => {
-        // TODO: Check if error code `FORBIDDEN` redirect to `/upgrade`
-        toast.error(error.message);
-      },
-    }),
-  );
+    };
 
-  const form = useForm<z.infer<typeof agentsInsertSchema>>({
-    resolver: zodResolver(agentsInsertSchema),
-    defaultValues: {
-      name: initialValues?.name ?? "",
-      instructions: initialValues?.instructions ?? "",
-    },
-  });
-
-  const isEdit = !!initialValues?.id;
-  const isPending = !!createAgent.isPending;
-
-  const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
-    if (isEdit) {
-      console.log("TODO: updateAgent");
-    } else {
-      createAgent.mutate(values);
-    }
-  };
-
-  return (
-    <Form {...form}>
-      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-        <GeneratedAvatar
-          seed={form.watch("name")}
-          varient="botttsNeutral"
-          className="border size-16"
-        />
-        <FormField
-          name="name"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder="e.g. Math tutor" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          name="instructions"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Instructions</FormLabel>
-              <FormControl>
-                <Textarea
-                  {...field}
-                  placeholder="You are a helpful math tutor who can explain complex ideas using simple and intuitive reasoning."
+    return (
+        <Form {...form}>
+            <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <GeneratedAvatar 
+                seed={form.watch("name")} 
+                 varient="botttsNeutral"
+                className="border size-16" />
+                <FormField
+                    name="name"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input {...field} placeholder="e.g Math tutor" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-between gap-x-2">
-          {onCancel && (
-            <Button
-              variant="ghost"
-              disabled={isPending}
-              type="button"
-              onClick={() => onCancel()}
-            >
-              Cancel
-            </Button>
-          )}
-          <Button disabled={isPending} type="submit">
-            {isEdit ? "Update" : "Create"}
-          </Button>
-        </div>
-      </form>
-    </Form>
-  );
+                <FormField
+                    name="instructions"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Instructions</FormLabel>
+                            <FormControl>
+                                <Textarea {...field} placeholder="e.g Assistant to help with Math" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="flex justify-between gap-x-2">
+                    {onCancel && (
+                        <Button variant="ghost" disabled={isPending} type="button" onClick={() => onCancel()}>
+                            Cancel
+                        </Button>
+                    )}
+                    <Button disabled={isPending} type="submit">
+                        {isEdit ? "Update" : "Create"}
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    );
 };
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+    useMutation,
+    useQueryClient,
+    useSuspenseQuery,
+} from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { LoadingState } from "@/components/loading-states";
@@ -8,66 +12,117 @@ import { ErrorState } from "@/components/error-state";
 import { GeneratedAvatar } from "@/components/generated-avater";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { agents } from "@/db/schema";
+import { useConfirm } from "../../hooks/use-confirm";
+import { UpdateAgentDialog } from "../components/update-agents-dialog";
+import { useState } from "react";
 
 interface Props {
-  agentId: string;
+    agentId: string;
 }
 
 export const AgentIdView = ({ agentId }: Props) => {
-  const trpc = useTRPC();
-  const { data } = useSuspenseQuery(
-    trpc.agents.getOne.queryOptions({ id: agentId }),
-  );
+    const trpc = useTRPC();
+    const router = useRouter();
+    const queryClient = useQueryClient();
+    const { data } = useSuspenseQuery(
+        trpc.agents.getOne.queryOptions({ id: agentId }),
+    );
+    const [UpdateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
+    const removeAgent = useMutation(
+        trpc.agents.remove.mutationOptions({
+            onSuccess: async () => {
+                (await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({}),
+                ),
+                    //TODO: invalidate free tier usage
+                    router.push("/agents"));
+            },
+            onError: (error) => {
+                toast.error(error.message);
+            },
+        }),
+    );
 
-  return (
-    <div className="flex-1 px-4 py-4 md:px-8 flex-col gap-y-4 flex">
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => {}}
-        onDelete={() => {}}
-      />
-      <div className="bg-white rounded-lg border">
-        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-          <div className="flex items-center gap-x-3">
-            <GeneratedAvatar
-              varient="botttsNeutral"
-              seed={data.name}
-              className="size-10"
+    const [RemoveConfirmation, confirmRemove] = useConfirm(
+        "Are you sure",
+        `The following action will remove ${data.meetingCount} associated meetings`,
+    );
+
+    const handleRemoveAgent = async () => {
+        const ok = await confirmRemove();
+
+        if (!ok) return;
+
+        await removeAgent.mutateAsync({ id: agentId });
+    };
+
+    return (
+        <>
+            <RemoveConfirmation />
+            <UpdateAgentDialog
+                open={UpdateAgentDialogOpen}
+                onOpenChange={setUpdateAgentDialogOpen}
+                intialValues={data}
             />
-            <h2 className="text-2xl font-medium">{data.name}</h2>
-          </div>
-          <Badge
-            variant="outline"
-            className="flex items-center gap-x-2 [&>svg]:size-4"
-          >
-            <VideoIcon className="text-blue-700" /> {data.meetingCount}{" "}
-            {data.meetingCount === 1 ? "meeting" : "meetings"}
-          </Badge>
-          <div className="flex flex-col gap-y-4">
-            <p className="text-lg font-medium">Instructions</p>
-            <p className="text-neutral-800">{data.instructions}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <AgentIdViewHeader
+                    agentId={agentId}
+                    agentName={data.name}
+                    onEdit={() => setUpdateAgentDialogOpen(true)}
+                    onDelete={handleRemoveAgent}
+                />
+                <div className="bg-white rounded-lg border">
+                    <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+                        <div className="flex items-center gap-x-3">
+                            <GeneratedAvatar
+                                varient="botttsNeutral"
+                                seed={data.name}
+                                className="size-10"
+                            />
+                            <h2 className="text-2xl font-medium">
+                                {data.name}
+                            </h2>
+                        </div>
+                        <Badge
+                            variant="outline"
+                            className="flex items-center gap-x-2 [&>svg]:size-4"
+                        >
+                            <VideoIcon className="text-blue-700" />
+                            {data.meetingCount}{" "}
+                            {data.meetingCount === 1 ? "meeting" : "meetings"}
+                        </Badge>
+                        <div className="flex flex-col gap-y-4">
+                            <div className="text-lg font-medium">
+                                Instructions
+                            </div>
+                            <div className="text-neutral-800">
+                                {data.instructions}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export const AgentIdViewLoading = () => {
-  return (
-    <LoadingState
-      title="Loading Agents"
-      description="This may take a while..."
-    />
-  );
+    return (
+        <LoadingState
+            title="Loading Agents"
+            description="This may take a while..."
+        />
+    );
 };
 
 export const AgentIdViewError = () => {
-  return (
-    <ErrorState
-      title="Error Loading Agents"
-      description="Please try again later"
-    />
-  );
+    return (
+        <ErrorState
+            title="Error Loading Agents"
+            description="Please try again later"
+        />
+    );
 };
