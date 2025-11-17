bf377737cf5c4369c294a33e91d9da3f5228a34a
Alif Jawad Mahi <mahi.alifjawad@gmail.com>
2025-11-17 21:02:48 +0600

    Meetings Form
    Added:
    1. `meetings.create` procedure.
    2. List Header.
    3. New Meeting Dialog.
    4. Meeting Form.
      - Command Select Component.
      - Reusable for create & delete.

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -10,6 +10,7 @@ import {
   MIN_PAGE_SIZE,
 } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
 
 export const meetingsRouter = createTRPCRouter({
   getMany: protectedProcedure
@@ -76,5 +77,41 @@ export const meetingsRouter = createTRPCRouter({
       }
 
       return existingMeetings;
+    }),
+
+  update: protectedProcedure
+    .input(meetingsUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [updatedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)),
+        )
+        .returning();
+
+      if (!updatedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return updatedMeeting;
+    }),
+
+  create: protectedProcedure
+    .input(meetingsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdMeeting] = await db
+        .insert(meetings)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      // TODO Create Stream Call, Upsert Stream Users
+      return createdMeeting;
     }),
 });
